0e27a4cf1b1ab13827df38a255293dff4094a3f5
fxhxyz <fxhxyz@proton.me>
2023-07-28 06:32:40 +0300

    Updates

---
+++ scripts/modules/refs.js
@@ -0,0 +1,8 @@
+const refs = {
+  cardList: document.querySelector('.cards-list'),
+  timeList: document.querySelectorAll('.time'),
+  addBtn: document.querySelector('#addBtn'),
+  allBtn: document.querySelector('#allBtn'),
+}
+
+export { refs };
