315a9522e5063db5ce38d829c82814436cfcda87
fxhxyz <fxhxyz@proton.me>
2023-09-16 06:50:51 +0300

    Updates

--- scripts/modules/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-  cardList: document.querySelector('.cards__list'),
-  timeList: document.querySelectorAll('.time__link'),
-  addBtn: document.querySelector('.btns__add'),
-  allBtn: document.querySelector('.btns__all'),
-}
-
-export { refs };
